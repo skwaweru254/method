b3a3dd68dc993cc5e1cf8da6cf4dccb60e066ca7
skwaweru254 <skwaweru254@gmail.com>
2022-11-18 15:35:38 +0300

    exactly two args

--- method.py
+++ method.py
@@ -1,6 +1,5 @@
-#   python method insert
-
-my_list = ['a','b','c','d']
-my_list.insert(100,'z')
+#   python insert method
+my_list = [1, 2, 3, 4, 5]
+my_list.insert(100, 22)  # insertion method need to args exactly
 
 print(my_list)
